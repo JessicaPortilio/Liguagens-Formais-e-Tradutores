e86fbde4d0a38da9a28cdc3288eff657372aaa61
JessicaPortilio <jessicaket91@gmail.com>
2023-01-20 23:59:57 -0300

    segundos exercicios em python

---
+++ Excercicio15.py
@@ -0,0 +1,20 @@
+# Escreva um programa que leia um valor inicial A 
+# e imprima a sequência de valores do cálculo do fatorial de A! e o seu resultado.
+# Por exemplo, imagine que o número lido seja 5 e teríamos o seguinte padrão de resposta:
+# 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+
+n = int(input('Informe um número: '))
+
+com = ''
+fatorial = 1
+nome = []
+print(n, '! = ', sep='', end='')
+while n > 0:
+    fatorial = fatorial * n
+    print(n , end='')
+    n-=1
+    if n >= 1:
+        print(' * ', end='')
+    
+print(' =', fatorial, end="")
