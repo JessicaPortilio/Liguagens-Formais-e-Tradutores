30966d132918b006fc4de593ec43d858ef236252
JessicaPortilio <jessicaket91@gmail.com>
2023-02-07 11:54:44 -0300

    resolução realizada pelo professor!

--- Compilador/Atividade_Entregar/Exercicio1.py
+++ Compilador/Atividade_Entregar/Exercicio1.py
@@ -1,47 +1,92 @@
-# Implementar um analisador léxico que reconheça qualquer sequência de palavras.
+import ply.lex as lex
 
-# 1.Para uma sequência de palavras pertencentes a uma mesma linha, deve ser gerado o Token Linha.
-# 2.Palavras que apresentam qualquer caractere diferente de letra, deve ser notificado erro léxico ao usuário
-# 3.O analisador deve dar suporte a avaliação de indentações/dedentações.
+def space_counter(token):
+    spaces = 0
+    for c in token.value:
+        if c == ' ':
+            spaces += 1
+        elif c == '\t':
+            spaces += 8 - (spaces % 8)
+    return spaces
 
-import ply.lex as lex # importando a biblioteca ply para o léxico
+tokens = ['LINHA', 'IDENT', 'DEDENT']
+stack = [0]
+states = (('idstate', 'exclusive'),
+          ('dedstate', 'exclusive'),)
+t_LINHA = '[a-zA-Z][a-zA-Z \t]+'
 
-#Definindo Tokens e seus padroes
-tokens = ("ID", "BRANCO", "DEDENT", "INDENT")
+def t_breakline(t):
+    r'\n+'                                 #recognizes one or more break lines
+    t.lexer.lineno += len(t.value) 
+    t.lexer.begin('idstate')
 
-t_ID = r'[a-zA-Z]+'  # Palavras que apresentam qualquer caractere
+def t_idstate_blankline(t):
+    r'([ \t]+)\n'                           #recognizes a blank line
+    # print('t_idstate_blankline')
+    pass
+
+def t_idstate_linewithcode(t):
+    '([ \t]+) | ([a-zA-Z])'                 #recognizes white spaces and tabs or a letter
+    # print('t_idstate_linewithcode')
+    n_spaces = space_counter(t)
+    t.lexer.begin('INITIAL')
+    if n_spaces < stack[-1]:
+        t.lexer.skip(-len(t.value))
+        stack.pop()
+        t.type='DEDENT'
+        t.lexer.begin('dedstate')
+        return t
+    elif n_spaces > stack[-1]:
+        stack.append(n_spaces)
+        t.type='IDENT'
+        return t
+    elif n_spaces == 0:
+        t.lexer.skip(-1)
+
+def t_dedstate_linewithdedent(t):
+    '([ \t]+) | ([a-zA-Z])'                 #recognizes white spaces and tabs or a letter
+    n_spaces = space_counter(t)
+    if n_spaces < stack[-1]:
+        t.lexer.skip(-len(t.value))
+        stack.pop()
+        t.type='DEDENT'
+        return t
+    elif n_spaces >= stack[-1]:  
+        t.lexer.begin('INITIAL')
+        if n_spaces > stack[-1]:
+            print('Erro de dedentação --->', n_spaces)
+        elif n_spaces == 0:                  # If the element starts with a letter
+            t.lexer.skip(-1)
 
 def t_error(t):
-    print("Error Léxico '%s'" % t.value[0])
+    print("ERROR in INITIAL state")
+    print(t.value)
     t.lexer.skip(1)
 
-def t_newline(t): #Adiciona a função t_newline
-     r'\n+' 
-     t.lexer.lineno += len(t.value) #Atualiza o contador de linha a 
-                                    #depender da quantidade de \n
+def t_idstate_error(t):
+    print("ERROR in idstate state")
+    t.lexer.skip(1)
 
-#Aqui tem uma função que pega os caracteres em branco
-num_count =0
-def t_BRANCO(t):
-    r'\s' # OBS: professor na dica o professor falou pra aqui ser "\t", mas quando eu coloco ele não funciona =(
-    global num_count
-    num_count += 1 # ele vai contando os caracter em branco
-    t.value = t.value
-    print(f'Contador: {num_count}') 
-    return t
-
-# Obs professor mesmo com a dica que o professor meu deu não conseguir fazer funcionar
+def t_dedstate_error(t):
+    print("ERROR in dedstate state")
+    t.lexer.skip(1)
 
 
-#t_ignore  = t_ignore  = ' \t' # Remove \n daqui
+lex.lex()
+programa = """cleardef perm
+    if len lsurf   
+                
+       return lalala land
+r sapore torf
+    for i in range len l  
+             s  lili lost
+             p perm
+             for x in pix
+                append tipo tor
+    return r
+"""
+lex.input(programa)
 
 
-#Criando analisador Lexico e realizando analise lexica
-lexer = lex.lex() 
-
-lexer.input("""def permif len
-def perm
-65657
-""") # Redefina o lexer e armazene uma nova string de entrada.
-for tok in lexer:
-  print(tok.type, tok.value, tok.lineno, tok.lexpos) 
+for token in lex.lexer:
+    print('[', token.type, ',', token.value, ']->', stack)
